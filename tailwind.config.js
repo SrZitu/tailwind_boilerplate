38f6408da656d14b3dd27855e859768bedc5e358
SrZitu <zitu094@gmail.com>
2023-04-21 11:27:35 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+//     screens:{
+// sm:'400px'
+//     },
+//     colors:{
+// 'green':'#22577A'
+//     akhne clor dile default color kaj korbena},
+    extend: {},
+  },
+  plugins: [],
+}
+
